506352069b72844d15fab15ec1045beab1252358
baru0103 <96758792+baru0103@users.noreply.github.com>
2023-10-11 22:56:05 +0900

    horse lighting fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,18 @@
 
 import { Suspense } from "react"
 import Link from "next/link"
+import { Physics, usePlane } from "@react-three/cannon"
 import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import Horse1 from "@/components/Horse1"
+import WaterSurface from "@/components/WaterSurface"
 
 export default function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10 grid-cols-1 md:grid-cols-2">
+    <section className="container grid grid-cols-1 items-center gap-5 pb-8 pt-6 md:grid-cols-2 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Beautifully designed components <br className="hidden sm:inline" />
@@ -45,8 +47,9 @@ export default function IndexPage() {
       <div className="flex h-[20rem] overflow-hidden sm:w-full md:h-[28rem] md:w-full">
         <Canvas>
           <OrbitControls enableZoom={false} />
-          <ambientLight intensity={0.7} />
-          <directionalLight position={[-3, 2, 5]} />
+          <ambientLight intensity={1.5} />
+          <directionalLight position={[1, 3, 2]} intensity={5} />
+
           <Suspense fallback={null}>
             <group position={[0, -1, 1]}>
               <Horse1 />
