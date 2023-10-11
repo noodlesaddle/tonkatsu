32a64d3a29b18bc9d75d7becef1f9888be49bf8b
baru0103 <96758792+baru0103@users.noreply.github.com>
2023-10-11 21:48:25 +0900

    horse 3d model added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+"use client"
+
+import { Suspense } from "react"
 import Link from "next/link"
+import { OrbitControls } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
+import Horse1 from "@/components/Horse1"
 
 export default function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10 grid-cols-1 md:grid-cols-2">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Beautifully designed components <br className="hidden sm:inline" />
@@ -15,24 +21,38 @@ export default function IndexPage() {
           Accessible and customizable components that you can copy and paste
           into your apps. Free. Open Source. And Next.js 13 Ready.
         </p>
+
+        <div className="flex gap-4">
+          <Link
+            href={siteConfig.links.docs}
+            target="_blank"
+            rel="noreferrer"
+            className={buttonVariants()}
+          >
+            Documentation
+          </Link>
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href={siteConfig.links.github}
+            className={buttonVariants({ variant: "outline" })}
+          >
+            GitHub
+          </Link>
+        </div>
       </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
+
+      <div className="flex h-[20rem] overflow-hidden sm:w-full md:h-[28rem] md:w-full">
+        <Canvas>
+          <OrbitControls enableZoom={false} />
+          <ambientLight intensity={0.7} />
+          <directionalLight position={[-3, 2, 5]} />
+          <Suspense fallback={null}>
+            <group position={[0, -1, 1]}>
+              <Horse1 />
+            </group>
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   )
---
+++ components/Horse1.tsx
@@ -0,0 +1,57 @@
+// @ts-nocheck
+"use client"
+
+import React, { useEffect, useRef } from "react"
+import { useAnimations, useGLTF } from "@react-three/drei"
+import { useThree } from "@react-three/fiber"
+
+export default function Model({ ...props }): any {
+  const group = useRef()
+  const { nodes, materials, animations } = useGLTF("/horse1.gltf")
+  const { actions } = useAnimations(animations, group)
+  const { camera } = useThree()
+
+  useEffect(() => {
+    camera.position.set(0, 2, 7) // Adjust the values as needed
+    camera.lookAt(1, 2, 2) // Adjust the target point as needed
+    const horseAction = actions.horse
+    horseAction.setEffectiveTimeScale(1.2)
+    // Play the animation
+    actions.horse.play()
+  }, [])
+
+  return (
+    <>
+      <group ref={group} {...props} dispose={null}>
+        <group name="Sketchfab_Scene">
+          <group
+            name="Sketchfab_model"
+            rotation={[-Math.PI / 1.8, 0, -1.6]}
+            scale={0.2}
+          >
+            <group name="b3c9bcb94f114ebeafddc7c3390b8d44abccleanermaterialmergergles">
+              <group name="Object_2" rotation={[Math.PI / 2.0, 1.2, 0]}>
+                <group name="MorphMainGroup">
+                  <mesh
+                    name="Horse1Shape"
+                    geometry={nodes.Horse1Shape.geometry}
+                    animations={true}
+                    material={materials.Horse1Shape}
+                    morphTargetDictionary={
+                      nodes.Horse1Shape.morphTargetDictionary
+                    }
+                    morphTargetInfluences={
+                      nodes.Horse1Shape.morphTargetInfluences
+                    }
+                  />
+                </group>
+              </group>
+            </group>
+          </group>
+        </group>
+      </group>
+    </>
+  )
+}
+
+useGLTF.preload("/horse1.gltf")
